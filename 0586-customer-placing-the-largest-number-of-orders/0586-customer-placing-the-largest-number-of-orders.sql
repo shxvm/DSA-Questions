81ff69575d196140360c0782b8a7163d75b3f5c7
Shivam Maurya <shxvm.maurya@gmail.com>
2022-10-18 15:27:40 +0530

    feat: add 0586-customer-placing-the-largest-number-of-orders solution

---
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select customer_number 
+from (select customer_number, count(order_number) order_count 
+from orders group by customer_number) a 
+order by order_count desc limit 1
